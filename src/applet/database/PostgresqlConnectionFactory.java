914283373fbaaf383d13d0e53228869ef78f9c46
lherloew <lherloew@gmail.com>
2011-05-19 15:27:31 +0200

    Deployable

--- src/applet/database/PostgresqlConnectionFactory.java
+++ src/applet/database/PostgresqlConnectionFactory.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  *
  */
 public class PostgresqlConnectionFactory {
-   private static final String URL = "jdbc:postgresql://192.168.1.10/nicolai3sf11";
+   private static final String URL = "jdbc:postgresql://192.168.1.11/nicolai3sf11";
    private static final String USERNAME = "nicolai3sf11";
    private static final String PASSWORD = "ihk100050eit";
 
--- src/beans/PostgresqlConnectionFactory.java
+++ src/beans/PostgresqlConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PostgresqlConnectionFactory {
-   private static final String URL = "jdbc:postgresql://192.168.1.10/nicolai3sf11";
+   private static final String URL = "jdbc:postgresql://192.168.1.11/nicolai3sf11";
    private static final String USERNAME = "nicolai3sf11";
    private static final String PASSWORD = "ihk100050eit";
 
